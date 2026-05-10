e77cb02c81244cccadcea501a4655ad9c901850c
João Vitor <olhierturcojoaovitor@gmail.com>
2026-05-10 18:10:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,24 @@ const senhaInput = document.getElementById("senha")// input que vai aparecer a s
 
 let statusMsg = document.getElementById("status")// card footer, onde ira aparecer a mensagem 
 
-function gerarSenha(){
-    const input = Number(document.getElementById("inputTamanho").value) // Passa o tamanho da senha a ser feita
-    const tamanho = input // Recebe o tamanho da senha a ser feita
+const input = document.getElementById("inputTamanho")// Passa o tamanho da senha a ser feita
 
+// Verifica se tem letras dentro do input e substitui por espaço em branco
+input.addEventListener("input", () => {
+  input.value = input.value.replace(/\D/g, "")
+})
+
+
+function gerarSenha(){
+
+  const valorTamanho = input.value
+  const tamanho = valorTamanho > 200 
+    ? (
+      alert("Atenção: limite excedido de 200 caracteres!"),
+      200
+    ) : (
+      valorTamanho
+    )
     const maiusculas = document.getElementById("maiusculas").checked
     const minusculas = document.getElementById("minusculas").checked
     const numeros = document.getElementById("numeros").checked
